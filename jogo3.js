021596eb48f1a07c60d3ee48fedf966156ff5317
Francisco Machado <franciscomachado9163@gmail.com>
2023-07-22 19:44:08 -0300

    qqr coisa

--- jogo3.js
+++ jogo3.js
@@ -1,7 +1,7 @@
 let numeros = []
 
 
-for (let i = 0; i < 7; i++) {
+for (let i = 0; i < 10; i++) {
     numeros.push(-10 + Math.floor(Math.random() * 30));
 }
 
@@ -9,11 +9,21 @@ console.log(numeros)
 
 let index = 0 
 
-setInterval(function(){
+var intervalo = setInterval(function(){
     document.getElementById("mensagem").innerHTML = numeros[index]
 
     index = (index + 1) % numeros.length
-}, 2000)
+
+    if (index === numeros.length - 1) {
+        clearInterval(intervalo);
+        document.getElementById("mensagem").innerHTML = ""
+        
+      }
+}, 1000)
+
+    
+
+
 
 let numero1= numeros[0]
 let numero2= numeros[1]
@@ -22,22 +32,29 @@ let numero4= numeros[3]
 let numero5= numeros[4]
 let numero6= numeros[5]
 let numero7= numeros[6]
+let numero8= numeros[7]
+
+console.log(numero1, numero2, numero3, numero4, numero5, numero6, numero7, numero8)
 
 
-let soma = (numero1 + numero2 + numero3 + numero4 + numero5 + numero6 + numero7)
-
-console.log(soma)
 
 
 function randomizarArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
+    const Tamanhoarray = array.length
+    for (let i = Tamanhoarray - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
+
+    
     }
     return array;
   }
 
+const Tamanhomaximo = 6
 
+let soma = (numero1 + numero2 + numero3 + numero4 + numero5 + numero6, numero7 + numero8)
+
+console.log(soma)
 
 let Respostas = [
 
@@ -50,8 +67,7 @@ soma * -1
 
 ]
 
-let RespostasNacaixa = randomizarArray(Respostas)
-
+const RespostasNacaixa = randomizarArray(Respostas)
 document.getElementById("caixa1").innerHTML = RespostasNacaixa[0]
 document.getElementById("caixa2").innerHTML = RespostasNacaixa[1]
 document.getElementById("caixa3").innerHTML = RespostasNacaixa[2]
@@ -74,26 +90,3 @@ document.getElementById("caixa6").innerHTML = RespostasNacaixa[5]
       }
     });
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
